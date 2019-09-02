cf24658d8b7c4b553f812f108830b17566c280ec
Mariusz Baran <mariusz.baran23@gmail.com>
2019-09-02 20:47:39 +0200

    Change products list item styles

--- shoper-shop/main/public/frontend-core/src/components/ProductsList/ProductsListItem.vue
+++ shoper-shop/main/public/frontend-core/src/components/ProductsList/ProductsListItem.vue
@@ -4,13 +4,40 @@
       class="products-list-item__link"
       :to="productUrl"
     >
-      <img
-        class="products-list-item__image"
-        :src="product.images[0].url"
-        :alt="product.name"
-      />
-      <h3 class="products-list-item__name">{{ product.name }}</h3>
-      <p class="products-list-item__price">{{ product.price }}</p>
+      <div class="products-list-item__image-wrapper">
+        <img
+          class="products-list-item__image"
+          :src="product.images[0].url"
+          :alt="product.name"
+        />
+        <span
+          v-if="!available"
+          class="products-list-item__image-badge"
+        >
+          Out of stock
+        </span>
+      </div>
+      <div class="products-list-item__details">
+        <div class="products-list-item__header">
+          <div class="products-list-item__header-left">
+            <h3 class="products-list-item__name">{{ product.name }}</h3>
+            <h4 class="products-list-item__category">{{ product.category.name }}</h4>
+          </div>
+          <div class="products-list-item__header-right">
+            <p class="products-list-item__price">{{ product.price }}</p>
+          </div>
+        </div>
+        <ul class="products-list-item__variations">
+          <li
+            v-for="stock in product.stocks"
+            :key="stock.id"
+            class="products-list-item__variation"
+            :class="{'products-list-item__variation--disabled': stock.amount === 0}"
+          >
+            {{ stock.variation.name }}
+          </li>
+        </ul>
+      </div>
     </router-link>
   </li>
 </template>
@@ -39,12 +66,22 @@ export default {
           id: this.product.id
         }
       };
+    },
+    available() {
+      for (let i = 0; i < this.product.stocks.length; ++i) {
+        if (this.product.stocks[i].amount > 0) {
+          return true;
+        }
+      }
+
+      return false;
     }
   }
 };
 </script>
 
 <style lang="scss">
+@import '../../utils/scss/variables/fonts';
 @import '../../utils/scss/variables/colors';
 
 .products-list-item {
@@ -54,14 +91,72 @@ export default {
     color: getColor('productBoxLink');
   }
 
+  &__image-wrapper {
+    position: relative;
+  }
+
   &__image {
     display: block;
     width: 100%;
     height: auto;
   }
 
-  &__name {
+  &__image-badge {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    max-width: 50%;
+    font-size: $fontSizeXSmall;
+    font-weight: $fontWeightRegular;
+    padding: .3rem;
+    background: getColor('productBadgeBackground');
+    border: 1px solid getColor('productBadgeBorder');
+  }
+
+  &__details {
     padding: 1rem;
+  }
+
+  &__header {
+    display: flex;
+    margin-bottom: 1rem;
+  }
+
+  &__header-left {
+    flex: 1;
+  }
+
+  &__name,
+  &__price {
+    font-size: $fontSizeMedium;
+    font-weight: $fontWeightMedium;
+  }
+
+  &__category {
+    font-size: $fontSizeRegular;
+    font-weight: $fontWeightRegular;
+    line-height: 1.5;
+  }
+
+  &__variations {
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &__variation {
+    padding: .2rem .5rem;
+    font-size: $fontSizeSmall;
+    font-weight: $fontWeightMedium;
+    border: 1px solid getColor('productBadgeBorder');
+
+    &:not(:last-of-type) {
+      margin-right: 1rem;
+    }
+
+    &--disabled {
+      opacity: 0.4;
+    }
   }
 }
 </style>
