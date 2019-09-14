58a2f069142dbed055b915da5ae33a7806dcd725
Mariusz Baran <mariusz.baran23@gmail.com>
2019-09-14 14:45:45 +0200

    Remove unnecessary console.log

--- shoper-shop/main/public/frontend-core/src/utils/js/scrollToElement.js
+++ shoper-shop/main/public/frontend-core/src/utils/js/scrollToElement.js
@@ -2,7 +2,5 @@ export function scrollToElement(element, offset = 0) {
   const headerHeight = document.querySelector('.main-header__navbar-wrapper').offsetHeight;
   const elementTop = element.getBoundingClientRect().top + window.pageYOffset;
 
-  console.log(elementTop, headerHeight, offset);
-
   window.scrollTo(0, elementTop - headerHeight - offset);
 }
